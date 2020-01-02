22cd7eabfd4929e775598c229e6634da4c00e228
duchiha <duchiha@example.com>
2020-01-02 04:37:36 +0100

    implement crud methods in the service

--- sample-app/src/app/services/product.service.ts
+++ sample-app/src/app/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from '../model/iproduct';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError, of } from 'rxjs';
 import { tap, catchError, map } from 'rxjs/operators'
 
 @Injectable({
@@ -9,26 +9,43 @@ import { tap, catchError, map } from 'rxjs/operators'
 })
 export class ProductService {
 
-  private productUrl: string = "api/products/products.json";
+  private productsUrl: string = "api/products";
 
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<IProduct[]> {
 
-    return this.http.get<IProduct[]>(this.productUrl).pipe(
+    return this.http.get<IProduct[]>(this.productsUrl).pipe(
       tap(data => console.log("All " + JSON.stringify(data))),
       catchError(this.handleError)
     );
 
   }
-
   getProduct(id: number): Observable<IProduct> {
-    return this.getProducts()
+    if (id === 0) {
+      return of(this.initializeProduct());
+    }
+    const url = `${this.productsUrl}/${id}`;
+    return this.http.get<IProduct>(url)
       .pipe(
-        map((products: IProduct[]) => products.find(p => p.productId === id))
+        tap(data => console.log('getProduct: ' + JSON.stringify(data))),
+        catchError(this.handleError)
       );
   }
 
+  private initializeProduct(): IProduct {
+    // Return an initialized object
+    return {
+      productId: 0,
+      productName: null,
+      productCode: null,
+      releaseDate: null,
+      price: null,
+      description: null,
+      starRating: null,
+      imageUrl: null
+    };
+  }
   private handleError(err: HttpErrorResponse) {
     // in a real world app, we may send the server to some remote logging infrastructure
     // instead of just logging it to the console
@@ -43,6 +60,38 @@ export class ProductService {
     }
     console.error(errorMessage);
     return throwError(errorMessage);
+  }
+
+  createProduct(product: IProduct): Observable<IProduct> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    product.productId = null;
+    return this.http.post<IProduct>(this.productsUrl, product, { headers })
+      .pipe(
+        tap(data => console.log('createProduct: ' + JSON.stringify(data))),
+        catchError(this.handleError)
+      );
+  }
+
+  deleteProduct(id: number): Observable<{}> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const url = `${this.productsUrl}/${id}`;
+    return this.http.delete<IProduct>(url, { headers })
+      .pipe(
+        tap(data => console.log('deleteProduct: ' + id)),
+        catchError(this.handleError)
+      );
+  }
+
+  updateProduct(product: IProduct): Observable<IProduct> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const url = `${this.productsUrl}/${product.productId}`;
+    return this.http.put<IProduct>(url, product, { headers })
+      .pipe(
+        tap(() => console.log('updateProduct: ' + product.productId)),
+        // Return the product on an update
+        map(() => product),
+        catchError(this.handleError)
+      );
   }
 
 }
